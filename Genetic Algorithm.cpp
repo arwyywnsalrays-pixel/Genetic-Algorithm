b46fc6c70e3bf180d9b53f61511b95ac1ac2d165
arwyywnsalrays-pixel <arwyywnsalrays@gmail.com>
2026-05-23 16:14:05 +0200

    Update Genetic Algorithm.cpp

--- Genetic Algorithm.cpp
+++ Genetic Algorithm.cpp
@@ -140,10 +140,81 @@ public:
         return child;
     }
 
+    // Mutation
+    void mutate(Queen &q) {
 
+        double p = (double)rand() / RAND_MAX;
 
+        if (p < mutRate) {
 
+            int col = rand() % n;
+            int row = rand() % n;
 
+            q.board[col] = row;
+
+            q.fit = fitness(q.board);
+        }
+    }
+
+    // Solve
+    Queen solve(bool useRoulette, ofstream &file) {
+
+        startPop();
+
+        Queen best(n);
+
+        for (int g = 1; g <= maxGen; g++) {
+
+            for (int i = 0; i < pop.size(); i++) {
+
+                if (pop[i].fit > best.fit)
+                    best = pop[i];
+            }
+
+            // Save Fitness Values
+           file << g << " "
+     << pop[0].fit
+     << endl;
+
+            // Max Fitness = 28
+            if (best.fit == 28) {
+
+                cout << "Generation: "
+                     << g << endl;
+
+                return best;
+            }
+
+            vector<Queen> newPop;
+
+            while (newPop.size() < popSize) {
+
+                Queen p1(n), p2(n);
+
+                if (useRoulette) {
+
+                    p1 = roulette();
+                    p2 = roulette();
+                }
+                else {
+
+                    p1 = tournament();
+                    p2 = tournament();
+                }
+
+                Queen child = cross(p1, p2);
+
+                mutate(child);
+
+                newPop.push_back(child);
+            }
+
+            pop = newPop;
+        }
+
+        return best;
+    }
+};
 
 
 
@@ -166,4 +237,33 @@ int main() {
 
         Queen ans =
             ga.solve(rouletteMethod, file);
+        
+        cout << "Fitness: "
+             << ans.fit << endl;
+
+        cout << "Board: ";
+
+        file << "Test " << t << endl;
+
+        for (int i = 0;
+             i < ans.board.size();
+             i++) {
+
+            cout << ans.board[i] << " ";
+
+            file << ans.board[i] << " ";
+        }
+
+        cout << endl << endl;
+
+        file << endl;
+
+        file << "Fitness: "
+             << ans.fit
+             << endl << endl;
+    }
+
+    file.close();
+
+    return 0;
     }
